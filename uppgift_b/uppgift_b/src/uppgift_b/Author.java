6a53e155440203b6f4a44ed9c0716c5c45354699
Fredrik <Fredrik@Fredriks-Lenovo>
2015-09-22 16:53:51 +0200

    test1

---
+++ uppgift_b/uppgift_b/src/uppgift_b/Author.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uppgift_b;
+
+/**
+ *
+ * @author anton
+ */
+public class Author {
+    private String name;
+    
+    public Author(String name){
+        this.name = name;
+    }
+    
+}
